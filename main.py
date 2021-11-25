0db42ccff0d9c02c885d53aaa7d3f914312b33ad
Ninjabsent <raphael.pietrzak@etu.univ-cotedazur.fr>
2021-11-25 18:21:21 +0100

    - Removed the unused class Label

--- main.py
+++ main.py
@@ -5,22 +5,7 @@ from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtWidgets import QFileDialog
 
 
-# https://stackoverflow.com/a/44508342
-class Label(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent=parent)
-        self.p = QtGui.QPixmap()
-
-    def setPixmap(self, p):
-        self.p = p
-        self.update()
-
-    def paintEvent(self, event):
-        if not self.p.isNull():
-            painter = QtGui.QPainter(self)
-            painter.setRenderHint(QtGui.QPainter.SmoothPixmapTransform)
-            painter.drawPixmap(self.rect(), self.p)
-
+# # https://stackoverflow.com/a/44508342
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
