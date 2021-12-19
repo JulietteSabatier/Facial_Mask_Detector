4258748de99da2ffeae7e254e4faf8516acb5a35
Ninjabsent <raphael.pietrzak@etu.univ-cotedazur.fr>
2021-12-19 18:28:49 +0100

    - Can now remove the last created Annotation with a right click (3 right clicks will remove the 3 last annotations)

--- View/custom_scene.py
+++ View/custom_scene.py
@@ -1,5 +1,6 @@
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QGraphicsRectItem
 
 from Model.selection_box import Box
 from Model.annotate_image import AnnotateImage
@@ -8,24 +9,42 @@ from Model.annotation import Annotation
 
 class CustomScene(QtWidgets.QGraphicsScene):
     currentAnnotateImage: AnnotateImage
+    rectangle_list: list[QGraphicsRectItem]
+    left_click_pressed: bool
 
     def __init__(self, parentWidget:QtWidgets.QWidget, parent=None):
         QtWidgets.QGraphicsScene.__init__(self, parent)
         self.currentBox = None
         self.parentWidget = parentWidget
         self.parentWidget.setCursor(Qt.CrossCursor)
+        self.left_click_pressed = False
+        self.rectangle_list = []
 
     def mousePressEvent(self, event:QtWidgets.QGraphicsSceneMouseEvent) -> None:
-        self.currentBox = Box(self, event.scenePos().x(), event.scenePos().y())
+        if event.button() == Qt.LeftButton:
+            self.currentBox = Box(self, event.scenePos().x(), event.scenePos().y())
+            self.left_click_pressed = True
+
+        elif event.button() == Qt.RightButton:
+
+            if len(self.rectangle_list) > 0:
+                self.removeItem(self.rectangle_list[-1])
+                self.rectangle_list.pop()
+
+            if len(self.currentAnnotateImage.get_annotation_list()) > 0:
+                self.currentAnnotateImage.get_annotation_list().pop()
 
     def mouseMoveEvent(self, event:QtWidgets.QGraphicsSceneMouseEvent) -> None:
-        self.currentBox.updateBottomRight(event.scenePos().x(), event.scenePos().y())
-        self.currentBox.update()
+        if self.left_click_pressed:
+            self.currentBox.updateBottomRight(event.scenePos().x(), event.scenePos().y())
+            self.currentBox.update()
 
     def mouseReleaseEvent(self, event:QtWidgets.QGraphicsSceneMouseEvent) -> None:
-        self.currentBox.updateBottomRight(event.scenePos().x(), event.scenePos().y())
-        self.currentBox.update()
-        self.finishBox()
+        if event.button() == Qt.LeftButton:
+            self.currentBox.updateBottomRight(event.scenePos().x(), event.scenePos().y())
+            self.currentBox.update()
+            self.finishBox()
+            self.left_click_pressed = False
 
     def wheelEvent(self, event:QtWidgets.QGraphicsSceneWheelEvent) -> None:
         # Do nothing, no scrolling allowed here sir (but later we'll maybe use it to zoom in/out
@@ -35,9 +54,9 @@ class CustomScene(QtWidgets.QGraphicsScene):
         if self.currentBox is not None:
             # Le mieux serait de faire un pop-up qui permette de donner un titre
             finalBox = self.currentBox.getBox()
+            self.rectangle_list.append(finalBox)
             title = "Default title"
             self.currentAnnotateImage.add_annotation(Annotation(title, finalBox))
-            self.currentBox = None
 
     def setCurrentAnnotateImage(self, annotateImage:AnnotateImage):
         self.currentAnnotateImage = annotateImage
