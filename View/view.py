6f380ef5e577371f34d6e865221ccc70358718ab
Ninjabsent <raphael.pietrzak@etu.univ-cotedazur.fr>
2021-12-23 15:48:32 +0100

    - If an annotation covers 20% or more of another annotation, or if more than 20% of the annotation is covering another one, it will now be discarded

--- View/view.py
+++ View/view.py
@@ -1,10 +1,19 @@
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt
 
+from View.custom_scene import CustomScene
+
 
 class View(QtWidgets.QGraphicsView):  # view of the image
+    scene: CustomScene
+
     def __init__(self, scene):
         QtWidgets.QGraphicsView.__init__(self, scene)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.scene = scene
+
+    def check_pixel_size(self):
+        scene_box = self.scene.getCurrentBox()
+        view_box = self.mapFromScene(scene_box)
+        print(view_box)
